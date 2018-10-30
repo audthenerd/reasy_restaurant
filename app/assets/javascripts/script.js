a4e4d6bca49428c35954d671238e54defdf5f36e
wang0805 <wh_wang@live.com>
2018-10-31 03:27:02 +0800

    changes made related to geocoding

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -12,7 +12,7 @@
 
 
 window.onload = function() {
-    if($("#customer_lat").length){
+    if($("#customer_latitude").length){
         getLocation();
     }
 }
@@ -25,75 +25,77 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    document.querySelector('#customer_lat').value = position.coords.latitude;
-    document.querySelector('#customer_long').value = position.coords.longitude;
+    document.querySelector('#customer_latitude').value = position.coords.latitude;
+    document.querySelector('#customer_longitude').value = position.coords.longitude;
 }
 
-function accessDatabase() {
-    let currentuserLat = readCookie('latitude');
-    let currentuserLong = readCookie('longitude');
+// function accessDatabase() {
+//     let currentuserLat = readCookie('latitude');
+//     let currentuserLong = readCookie('longitude');
 
-    var ajaxUrlOne = 'http://127.0.0.1:3000/post/sort/db';
-    var responseHandlerOne = function() {
+//     var ajaxUrlOne = 'http://127.0.0.1:3000/post/sort/db';
+//     var responseHandlerOne = function() {
 
-        var responseObjOne = JSON.parse(this.responseText);
+//         var responseObjOne = JSON.parse(this.responseText);
 
-        for (var i = 0; i < responseObjOne.rows.length; i++) {
-            var ajaxUrlTwo = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + (currentuserLong) + ',' + (currentuserLat) + ';' + (responseObjOne.rows[i].loclong) + ',' + (responseObjOne.rows[i].loclat) + '?access_token=pk.eyJ1IjoiY2xhcmFvbm1hcnMiLCJhIjoiY2puOHgxdnhlMGk5bTN0bnc2b2JiaXhuZSJ9.Bbd0prUBoCltNk8UOyFD0Q'
+//         for (var i = 0; i < responseObjOne.rows.length; i++) {
+//             var ajaxUrlTwo = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + (currentuserLong) + ',' + (currentuserLat) + ';' + (responseObjOne.rows[i].loclong) + ',' + (responseObjOne.rows[i].loclat) + '?access_token=pk.eyJ1IjoiY2xhcmFvbm1hcnMiLCJhIjoiY2puOHgxdnhlMGk5bTN0bnc2b2JiaXhuZSJ9.Bbd0prUBoCltNk8UOyFD0Q'
 
-            console.log(ajaxUrlTwo);
-            let numOne = responseObjOne.rows[i].post_id;
+//             console.log(ajaxUrlTwo);
+//             let numOne = responseObjOne.rows[i].post_id;
 
-            var responseHandlerTwo = function() {
+//             var responseHandlerTwo = function() {
 
-                var responseObjTwo = JSON.parse(this.responseText);
-                console.log('isthisworking:',responseObjTwo)
-                let numTwo = responseObjTwo.routes[0].duration;
+//                 var responseObjTwo = JSON.parse(this.responseText);
+//                 console.log('isthisworking:',responseObjTwo)
+//                 let numTwo = responseObjTwo.routes[0].duration;
 
-                var ajaxUrlThree = 'http://127.0.0.1:3000/post/sort/loc?id=' + numOne + '&duration=' + numTwo;
+//                 var ajaxUrlThree = 'http://127.0.0.1:3000/post/sort/loc?id=' + numOne + '&duration=' + numTwo;
 
-                var responseHandlerThree = function() {
-                    var responseObjThree = JSON.parse(this.responseText);
-                    console.log('whatabouthtis:',responseObjThree)
-
-
-                };
-                var requestThree = new XMLHttpRequest();
-                requestThree.addEventListener("load", responseHandlerThree);
-                requestThree.open("GET", ajaxUrlThree);
-                requestThree.send();
+//                 var responseHandlerThree = function() {
+//                     var responseObjThree = JSON.parse(this.responseText);
+//                     console.log('whatabouthtis:',responseObjThree)
 
 
-
-            }
-            var requestTwo = new XMLHttpRequest();
-            requestTwo.addEventListener("load", responseHandlerTwo);
-            requestTwo.open("GET", ajaxUrlTwo);
-            requestTwo.send();
-        }
-
-    };
-    var requestOne = new XMLHttpRequest();
-    requestOne.addEventListener("load", responseHandlerOne);
-    requestOne.open("GET", ajaxUrlOne);
-    requestOne.send();
-}
-
-function saveLocation() {
-    var input = document.getElementById('restaurant_location');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        var place = autocomplete.getPlace();
-        var lat = place.geometry.location.lat();
-        var long = place.geometry.location.lng()
+//                 };
+//                 var requestThree = new XMLHttpRequest();
+//                 requestThree.addEventListener("load", responseHandlerThree);
+//                 requestThree.open("GET", ajaxUrlThree);
+//                 requestThree.send();
 
 
-        document.getElementById('restaurant_location').value = place.name;
-        document.getElementById('restaurant_lat').value = lat;
-        document.getElementById('restaurant_long').value = long;
-        //alert("This function is working!");
-        //alert(place.name);
-        // alert(place.address_components[0].long_name);
 
-    });
-}google.maps.event.addDomListener(window, 'load', saveLocation);
+//             }
+//             var requestTwo = new XMLHttpRequest();
+//             requestTwo.addEventListener("load", responseHandlerTwo);
+//             requestTwo.open("GET", ajaxUrlTwo);
+//             requestTwo.send();
+//         }
+
+//     };
+//     var requestOne = new XMLHttpRequest();
+//     requestOne.addEventListener("load", responseHandlerOne);
+//     requestOne.open("GET", ajaxUrlOne);
+//     requestOne.send();
+// }
+
+// function saveLocation() {
+//     var input = document.getElementById('restaurant_location');
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+//     google.maps.event.addListener(autocomplete, 'place_changed', function() {
+//         var place = autocomplete.getPlace();
+//         var lat = place.geometry.location.lat();
+//         var long = place.geometry.location.lng()
+
+
+//         document.getElementById('restaurant_location').value = place.name;
+//         document.getElementById('restaurant_lat').value = lat;
+//         document.getElementById('restaurant_long').value = long;
+//         //alert("This function is working!");
+//         //alert(place.name);
+//         // alert(place.address_components[0].long_name);
+
+//     });
+// }
+
+// google.maps.event.addDomListener(window, 'load', saveLocation);
