324c6b6d9cfcbe9f33fe5100645e4c2eeff19781
audthenerd <aud.kow88@gmail.com>
2018-10-30 10:40:43 +0800

    added qty col to reserve_menuitems table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_020907) do
+ActiveRecord::Schema.define(version: 2018_10_30_023802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_020907) do
   create_table "reservations_menuitems", id: false, force: :cascade do |t|
     t.bigint "reservation_id"
     t.bigint "menuitem_id"
+    t.integer "quantity"
     t.index ["menuitem_id"], name: "index_reservations_menuitems_on_menuitem_id"
     t.index ["reservation_id"], name: "index_reservations_menuitems_on_reservation_id"
   end
