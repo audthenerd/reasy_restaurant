6a13348c03171bccdd4d9ab87e4c1e24c2894c68
wang0805 <wh_wang@live.com>
2018-11-01 15:45:59 +0800

    hello

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,31 @@
 
 Category.create([{name: 'Japanese', image_url: 'jap_food.jpg'}, {name: 'Korean', image_url: 'korean_food.jpg'}, {name: 'Chinese', image_url: 'chinese_food.jpg'}, {name: 'Italian', image_url: 'italian_food.jpg'}, {name: 'French', image_url: 'french_food.jpg'}])
 
-Userrest.create(email: "wh_wang@live.com", encrypted_password: "$2a$11$urCx145J.dXM4JVtKOKiI.6Pjku1fsGjgXQbKa4zb2.1FQpLS9ZvK").first_or_create(email: "wh_wang@live.com")
 
-# Restaurant.create(name: "firstrestro", ... )
+Userrest.create(email: "wenhao@live.com", password: "password")
+Userrest.create(email: "audrey@gmail.com", password: "password")
+Userrest.create(email: "clara@live.com", password: "password")
+
+Restaurant.create(name: "firstrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 1)
+Restaurant.create(name: "secrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 2)
+Restaurant.create(name: "thirdrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 3)
+Restaurant.create(name: "fourthrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 1)
+Restaurant.create(name: "fifthrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 2)
+Restaurant.create(name: "sixthrestro", street: "1 hougang street 91", city: "singapore", zip: "538692", userrest_id: 3)
+
+Customer.create(name:"wenhao", email:"what@gmail.com", password: "password", image_url: "fa3ingoojck75cf5akgw")
+Customer.create(name:"audrey", email:"the@gmail.com", password: "password", image_url: "fa3ingoojck75cf5akgw")
+Customer.create(name:"clara", email:"hell@gmail.com", password: "password", image_url: "fa3ingoojck75cf5akgw")
+
+Menuitem.create(item: "oneitem", price: 10.00, restaurant_id: 1)
+Menuitem.create(item: "twoitem", price: 10.00, restaurant_id: 1)
+Menuitem.create(item: "threeitem", price: 10.00, restaurant_id: 1)
+Menuitem.create(item: "fouritem", price: 10.00, restaurant_id: 1)
+Menuitem.create(item: "oneitem", price: 10.00, restaurant_id: 2)
+Menuitem.create(item: "twoitem", price: 10.00, restaurant_id: 2)
+Menuitem.create(item: "threeitem", price: 10.00, restaurant_id: 2)
+Menuitem.create(item: "fouritem", price: 10.00, restaurant_id: 2)
+Menuitem.create(item: "oneitem", price: 10.00, restaurant_id: 3)
+Menuitem.create(item: "twoitem", price: 10.00, restaurant_id: 3)
+Menuitem.create(item: "threeitem", price: 10.00, restaurant_id: 3)
+Menuitem.create(item: "fouritem", price: 10.00, restaurant_id: 3)
