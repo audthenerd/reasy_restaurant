d7f0cb30255869444698899aa9a6c9d569721627
audthenerd <aud.kow88@gmail.com>
2018-11-02 18:07:28 +0800

    two graphs and reservation seeds added

--- app/models/menuitem.rb
+++ app/models/menuitem.rb
@@ -1,4 +1,6 @@
 class Menuitem < ApplicationRecord
   belongs_to :restaurant
-  has_and_belongs_to_many :reservations
+  has_many :menuitems_reservations
+  has_many :reservations, through: :menuitems_reservations
+  # has_and_belongs_to_many :reservations
 end
